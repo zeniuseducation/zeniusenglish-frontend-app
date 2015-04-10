bf815580474426748c532941f83413ff6cb139cc
Wisnu Mulya <wisnu.p.mulya@gmail.com>
2015-04-11 01:08:20 +0700

    Modify stylings and scriptings

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,10 @@ var burger = document.getElementById('burger');
 var search = document.getElementById('search');
 var nav = document.getElementsByTagName('nav');
 var searchInput = document.getElementById('search-input');
+var searchDoc = document.querySelectorAll('[type="search"]');
+var filterDoc = document.querySelectorAll('[type="filter"]');
+var searchDocInput = document.getElementById('searchDoc');
+var filterDocSelect = document.getElementById('filterDoc');
 
 function toggleNav() {
   nav[0].style.display = nav[0].style.display === 'none'? 'block': 'none';
@@ -18,5 +22,21 @@ function toggleSearch() {
   }
 }
 
+function toggleInputFilter() {
+    searchDocInput.style.display = searchDocInput.style.display === 'none'? 'block' : 'none';
+    if(filterDocSelect.style.display === 'block') {
+        filterDocSelect.style.display = 'none';
+    }
+}
+
+function toggleSelectFilter() {
+    filterDocSelect.style.display = filterDocSelect.style.display === 'none'? 'block' : 'none';
+    if(searchDocInput.style.display === 'block') {
+        searchDocInput.style.display = 'none';
+    }
+}
+
 burger.addEventListener('click', toggleNav);
 search.addEventListener('click', toggleSearch);
+searchDoc[1].addEventListener('click', toggleInputFilter);
+filterDoc[0].addEventListener('click', toggleSelectFilter);
