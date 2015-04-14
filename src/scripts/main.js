e11dfac52ad5352dcca00ad71b7dada5b4525a8f
Wisnu Mulya <wisnu.p.mulya@gmail.com>
2015-04-14 07:26:57 +0700

    Refactor dropdown functionality in main.js and adjust the changes to html and scss

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,46 +1,25 @@
 'use strict';
-var burger = document.getElementById('burger');
-var search = document.getElementById('search');
-var nav = document.getElementsByTagName('nav');
-var searchInput = document.getElementById('search-input');
-var searchDoc = document.querySelectorAll('[type="search"]');
-var filterDoc = document.querySelectorAll('[type="filter"]');
-var searchDocInput = document.getElementById('searchDoc');
-var filterDocSelect = document.getElementById('filterDoc');
 
-function toggleNav() {
-  nav[0].style.display = nav[0].style.display === 'none'? 'block': 'none';
-  if (searchInput.style.display === 'block') {
-    searchInput.style.display = 'none';
-  }
+var doc = window.document;
+var dropdownToggleElements = doc.querySelectorAll('[data-toggle="dropdown"]');
+
+// Show a target element when trigger element is clicked then hide targetHide element
+function dropdownToggle(trigger, targetShow, targetHide) {
+  trigger.onclick = function() {
+    console.log('works');
+    targetShow.style.display = targetShow.style.display === 'none'? 'block': 'none';
+    if (targetHide.style.display === 'block') {
+      targetHide.style.display = 'none';
+    }
+  };
 }
 
-function toggleSearch() {
-  searchInput.style.display = searchInput.style.display === 'none'? 'block': 'none';
-  if (nav[0].style.display === 'block') {
-    nav[0].style.display = 'none';
+// Map all element with data-toggle attribute valued dropdown
+if (dropdownToggleElements) {
+  for (var i = 0; i < dropdownToggleElements.length; i++) {
+    dropdownToggle(
+      dropdownToggleElements[i],
+      doc.querySelectorAll(dropdownToggleElements[i].dataset.target)[0],
+      doc.querySelectorAll(dropdownToggleElements[i].dataset.hide)[0]);
   }
 }
-
-function toggleInputFilter() {
-  searchDocInput.style.display = searchDocInput.style.display === 'none'? 'block' : 'none';
-  if(filterDocSelect.style.display === 'block') {
-    filterDocSelect.style.display = 'none';
-  }
-}
-
-function toggleSelectFilter() {
-  filterDocSelect.style.display = filterDocSelect.style.display === 'none'? 'block' : 'none';
-  if(searchDocInput.style.display === 'block') {
-    searchDocInput.style.display = 'none';
-  }
-}
-
-burger.addEventListener('click', toggleNav);
-search.addEventListener('click', toggleSearch);
-searchDoc[1].addEventListener('click', toggleInputFilter);
-filterDoc[0].addEventListener('click', toggleSelectFilter);
-
-window.onscroll = function() {
-  document.getElementById('back-top').style.display = window.pageYOffset >= 50? 'inline-block' : 'none';
-};
